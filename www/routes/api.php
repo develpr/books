bb4b30d9241e1068745a70edeb9f12572f4b1d9f
Kevin Mitchell <kevin@develpr.com>
2017-03-26 19:52:06 -0500

    initial commit

---
+++ www/routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::post('oauth/tokens', 'LoginController@login');
+});
+
+Route::group(['middleware' => ['auth:api']], function ($router) {
+    $router->get('oauth/scopes', [
+        'uses' => '\Laravel\Passport\Http\Controllers\ScopeController@all',
+    ]);
+
+    $router->get('oauth/tokens', [
+        'uses' => '\Laravel\Passport\Http\Controllers\PersonalAccessTokenController@forUser',
+    ]);
+
+    $router->delete('oauth/tokens/{token_id}', [
+        'uses' => '\Laravel\Passport\Http\Controllers\PersonalAccessTokenController@destroy',
+    ]);
+
+    $router->get('users/me', function(Request $request) {
+        return $request->user();
+    });
+});
